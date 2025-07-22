d0e778f46b319dcab5ec7827411804b28829fae5
Zharfan Dhiyaus Suruur <zharfan@rocketmail.com>
2025-07-22 16:51:13 +0700

    Update SQL_Query2_CreateDatabaseAdmin.sql

--- SQL_Query2_CreateDatabaseAdmin.sql
+++ SQL_Query2_CreateDatabaseAdmin.sql
@@ -12,7 +12,7 @@ CREATE TABLE Users (
 GO
 
 INSERT INTO Users (Username, PasswordHash)
-VALUES ('admin', '$2b$12$l1NjX9muUx0EwvwXGCM5eOwm7H6lXtJ37NZsvM86PI7CTb4ydA312'); -- Password hash didapat dari registrasi_admin.py
+VALUES ('admin', '$2b$12$l1NjX9muUx0EwvwXGCM5eOwm7H6lXtJ37NZsvM86PI7CTb4ydA312'); -- Password hash didapat dari registrasi_admin.py password hash ini dapat dari 12345678
 GO
 
 SELECT * FROM Users;
