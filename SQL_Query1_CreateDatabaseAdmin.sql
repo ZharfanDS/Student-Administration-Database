8da235555bc5986399179436a4def27867cd9da7
Zharfan Dhiyaus Suruur <zharfan@rocketmail.com>
2025-07-22 16:09:08 +0700

    Create SQL_Query1_CreateDatabaseAdmin.sql

---
+++ SQL_Query1_CreateDatabaseAdmin.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE Admin_DB;
+GO
+
+USE Admin_DB;
+GO
+
+CREATE TABLE Users (
+  ID INT PRIMARY KEY IDENTITY(1,1),
+  Username VARCHAR(50) NOT NULL UNIQUE,
+  PasswordHash VARCHAR(100) NOT NULL -- Menyimpan password yang sudah dienkripsi
+  );
+GO
+
+INSERT INTO Users (Username, PasswordHash)
+VALUES ('admin', '$2b$12$l1NjX9muUx0EwvwXGCM5eOwm7H6lXtJ37NZsvM86PI7CTb4ydA312'); -- Password hash didapat dari registrasi_admin.py
+GO
+
+SELECT * FROM Users;
+GO
